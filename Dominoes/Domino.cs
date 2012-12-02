27f3ed237c3443fbd831ee2e78a27ea48ba3d793
Paul Miller <pmiller@microsoft.com>
2012-12-01 23:17:55 -0800

    intitial commit.

---
+++ Dominoes/Domino.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominoes
+{
+    class Domino
+    {
+        public int First { get; private set; }
+        public int Second { get; private set; }
+        public readonly static int MAX_DOTS;
+        public readonly static int MIN_DOTS;
+
+        public Domino(int one, int two)
+        {
+            First = one;
+            Second = two;
+
+            if (First > MAX_DOTS || First < 0)
+                throw new ArgumentOutOfRangeException(string.Format("first isn't a valid domino: {0}", First));
+            if (Second > MAX_DOTS || Second < 0)
+                throw new ArgumentOutOfRangeException(string.Format("second isn't a valid domino: {0}", Second));
+        }
+
+        public string ToString()
+        {
+            this.H
+            return string.Format("[{0}|{1}]", First, Second);
+        }
+
+        public int GetHashCode()
+        {
+            //good chance this is buggy
+            return (MAX_DOTS+1)*First + Second;
+        }
+    }
+}
---
+++ Dominoes/Tiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominoes
+{
+    class Tiles
+    {
+        private Queue<Domino> _tiles;
+        static readonly Random rnd = new Random();
+
+        public Tiles()
+        {
+            var dotrange = Enumerable.Range(Domino.MIN_DOTS, Domino.MAX_DOTS);
+            var initial = dotrange.SelectMany(f => Enumerable.Range(Domino.MIN_DOTS, f).Select(s => new Domino(f, s)))
+            var shuffled = initial.OrderBy(d => rnd.Next());
+            _tiles = new Queue<Domino>(shuffled);
+            
+        }
+
+        public bool Empty()
+        {
+            return !_tiles.Any();
+        }
+
+        public Domino Next()
+        {
+            return _tiles.Dequeue();
+        }
+
+    }
+}
